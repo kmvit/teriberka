b92ccfa3c8ede9d9f40e0784c64e067796480095
Андрей Галкин <justscoundrel@yandex.ru>
2025-11-13 18:24:39 +0300

    Добавил изменения

--- config/settings.py
+++ config/settings.py
@@ -179,12 +179,10 @@ FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:3000')
 # В режиме разработки (если EMAIL_HOST не указан) письма выводятся в консоль
 
 # SMTP настройки
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
-EMAIL_USE_TSL = os.getenv('EMAIL_USE_TSL')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
-SERVER_EMAIL = DEFAULT_FROM_EMAIL  # Email для отправки ошибок администратору
+EMAIL_HOST='smtp.yandex.ru'
+EMAIL_PORT=587
+EMAIL_USE_SSL=True
+EMAIL_USE_TSL=False
+EMAIL_HOST_USER='onbalidevelopment@yandex.ru'
+EMAIL_HOST_PASSWORD='Vhcrdoft123'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
