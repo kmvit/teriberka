3c7ebac95fde66457302816530c8127703ab3498
Андрей Галкин <justscoundrel@yandex.ru>
2026-01-15 20:14:48 +0300

    Удален debug endpoint - проблема решена

--- apps/trips/urls.py
+++ apps/trips/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views import AvailableTripsView, TripDetailView
-from .debug_time import DebugTimeView
 
 app_name = 'trips'
 
 urlpatterns = [
     path('', AvailableTripsView.as_view(), name='available-trips'),
-    path('debug-time/', DebugTimeView.as_view(), name='debug-time'),
     path('<int:trip_id>/', TripDetailView.as_view(), name='trip-detail'),
 ]
